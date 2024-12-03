14433410a89a40d365078b4eba83e229b1a59185
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-12-02 17:46:29 -0800

    fixed the ai problem

--- scripts/predictButton.js
+++ scripts/predictButton.js
@@ -2,16 +2,18 @@
 var predictButton = document.querySelector(".fancy-button");
 
 if (predictButton) {
-    predictButton.addEventListener("click", async function() {
-        try {
-            const response = await axios.post('http://localhost:3000/predictDamage', {disaster: disaserType});
-            const data = response.data;
-            alert(`Predicted Damage: ${data.prediction}`);
-        } catch (error) {
-            console.error('Error:', error);
-            alert('Failed to predict damage');
-        }
-    });
+  predictButton.addEventListener("click", async function () {
+    try {
+      const response = await axios.post("/predictDamage", {
+        disaster: disaserType,
+      });
+      const data = response.data;
+      alert(`Predicted Damage: ${data.prediction}`);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to predict damage");
+    }
+  });
 } else {
-    console.error("Predict button not found");
+  console.error("Predict button not found");
 }
